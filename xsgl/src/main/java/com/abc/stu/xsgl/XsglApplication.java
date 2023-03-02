892b83f83b83b1e0a6f26be6b24d75b13a58c63f
yang <767015425@qq.com>
2023-03-02 20:51:36 +0800

    第一次创建项目1

---
+++ xsgl/src/main/java/com/abc/stu/xsgl/XsglApplication.java
@@ -0,0 +1,13 @@
+package com.abc.stu.xsgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsglApplication.class, args);
+    }
+
+}
---
+++ xsgl/src/test/java/com/abc/stu/xsgl/XsglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.stu.xsgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
